cebcd504743805ea3d2cae3211adb54750f2b5fa
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2023-01-06 02:08:01 -0600

    Angular project for my portfolio webpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ContentsComponent } from './component/contents/contents.component';
+import { DreamComponent } from './component/dream/dream.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    NavbarComponent,
+    ContentsComponent,
+    DreamComponent
   ],
   imports: [
     BrowserModule,
